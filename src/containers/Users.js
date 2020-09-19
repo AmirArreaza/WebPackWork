77826a4875a08d6bf1c3aba1e5810b868d9400d7
AmirArreaza <ajaarreazav@gmail.com>
2020-09-19 20:16:08 +0100

    Basic React App created

---
+++ src/containers/Users.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Users extends Component {
+
+    render () {
+
+        return(
+            <div>
+                <h1>The Users</h1>
+                <p>Hola</p>
+            </div>
+        );
+    }
+}
+
+export default Users;
