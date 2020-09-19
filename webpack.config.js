848ba99c44dcf16e656a030e8a6eaae865b7dc89
AmirArreaza <ajaarreazav@gmail.com>
2020-09-19 20:40:43 +0100

    Added loading CSS files structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const autoprefixer = require("autoprefixer");
 
 module.exports = {
   mode: "development",
@@ -14,7 +15,25 @@ module.exports = {
       {
         test: /\.js$/,
         loader: "babel-loader",
-        exclude: /node_modules/
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: { localIdentName: "[name]__[local]__[hash:base64:5]" },
+            },
+          },
+          { loader: "postcss-loader", options: {
+            ident:'postcss',
+            plugins: () => [autoprefixer()]
+          } },
+        ],
       },
     ],
   },
